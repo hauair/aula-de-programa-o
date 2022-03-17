b0001663675da28bf2034907e26e0609ffb1d51e
THIAGO DO AMARAL CASTRO RODRIGUES <201902134877@alunos.estacio.br>
2022-03-17 19:38:22 -0300

    atualizacao3

--- App.js
+++ App.js
@@ -4,11 +4,15 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Thiago do Amaral Castro Rodrigues</Text>
+          <Text>Thiago do Amaral Castro Rodrigues</Text>
+          <Text>Resultado</Text>
+          <Text>Digite o valor 1</Text>
+          <Text>Digite o valor 2</Text>
+          <Text>Selecione a operação</Text>
       <StatusBar style="auto" />
 <Button
   
-  title="Resultado"
+  title="Calcular"
   color="#ff0000"
   accessibilityLabel="Learn more about this purple button"
 />
