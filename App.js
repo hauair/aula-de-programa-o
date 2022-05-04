a5ff7752379a63b2b30c9aac9ca4d6556b298303
Thiago Rodrigues <thiacrod13@gmail.com>
2022-05-04 20:40:17 -0300

    atualizacao7

--- App.js
+++ App.js
@@ -8,44 +8,43 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 export default function App() {
   const [darkMode, setdarkMode] = useState(false)
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    results:{
+      backgroundColor: darkMode ? "#282f3b" : "#f5f5f5",
+      width: '100%',
+      minHeight: 280,
+      alignItems: 'flex-end',
+      justifyContent: 'flex-end',
+ },
+ resultText: {
+   margin: 10,
+   fontSize: 25
+ },
+ themeButton: {
+   backgroundColor: darkMode ? "#7b8084" :"#e5e5e5"
+ }
+
+  });
   
   return (
     <View>
-    <View style={styles.results}>
-     <TouchableOpacity style={styles.themeButton}></TouchableOpacity>
-      </View>
-      <View style={styles.buttons}>
+     <View style={styles.results}>
+       <TouchableOpacity style={styles.themeButton}></TouchableOpacity>
+       <Text style={styles.resultText}>2 + 2 = 5</Text>
+     </View>
+     <View style={styles.buttons}>
       
       </View>
+     </View>
           
   );
 }
 
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#black',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  results:{
-    backgroundColor: darkMode ? "#282f3b" : "f5f5f5",
-    width: '100%',
-    minHeight: 300,
-
-  }
-
-});
